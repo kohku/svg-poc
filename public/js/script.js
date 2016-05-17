a07eafaf50a8d26999ec8604f3a8b67938afe6ba
Marhatta, Ramesh <ramesh.marhatta@one.verizon.com>
2016-05-17 12:52:01 -0500

    My first commit

---
+++ public/js/script.js
@@ -0,0 +1,10 @@
+function submit(){
+var hght = $("#hght_create").val().parseFloat();
+var wdth = $("#wdth_create").val().parseFloat();
+alert(hght);
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.open("POST", "/json-handler");
+xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+xmlhttp.send(JSON.stringify({ Height: hght, Width: wdth }));
+}
